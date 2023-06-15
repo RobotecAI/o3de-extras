794dd8ce3850ce6a192188c06109ee3e8a1a4511
Adam Krawczyk <adam-krawczyk@outlook.com>
2023-06-15 15:29:09 +0200

    Add extra slash
    Signed-off-by: Adam Krawczyk <adam-krawczyk@outlook.com>

--- Test/test_build.sh
+++ Test/test_build.sh
@@ -8,10 +8,10 @@ echo "Running test script"
 cd /data/workspace/WarehouseTest
 
 if cmake --build build/linux --config profile --target WarehouseTest.GameLauncher Editor ; then
-    VALUE="RESULT: ALL TESTS PASSED" # expected result
+    VALUE="\"RESULT: ALL TESTS PASSED"\" # expected result
     echo "Build succeeded"
 else
-    VALUE="RESULT: Build failed"
+    VALUE=""\RESULT: Build failed"\"
 fi
 
 # Print the value, needed for the action to get the result
