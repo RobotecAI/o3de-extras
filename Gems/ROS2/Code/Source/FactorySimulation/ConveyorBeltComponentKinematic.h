529fbcb97a19fd77f7922bb1a2b3ccd9b52e6370
Michał Pełka <michal.pelka@robotec.ai>
2023-07-06 21:16:16 +0200

    Fix spawning
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>

--- Gems/ROS2/Code/Source/FactorySimulation/ConveyorBeltComponentKinematic.h
+++ Gems/ROS2/Code/Source/FactorySimulation/ConveyorBeltComponentKinematic.h
@@ -36,7 +36,9 @@ namespace ROS2
         , public AZ::TickBus::Handler
         , public AZ::EntityBus::Handler
     {
-        static constexpr float SegmentWidth = 0.1f; //!< The width of the segment of the belt
+        static constexpr float SegmentWidth = 0.1f; //!< The width of the segment of the belt (in meters)
+        static constexpr float SegmentSeparation = 1.0f; //!< Separation between segments of the belt (in normalized units)
+
     public:
         AZ_COMPONENT(ConveyorBeltComponentKinematic, "{B7F56411-01D4-48B0-8874-230C58A578BD}");
         ConveyorBeltComponentKinematic() = default;
@@ -92,8 +94,9 @@ namespace ROS2
         //! Despawn segments that are at the end of the spline
         void DespawnSegments();
 
-        //! Spawn segments if there is a free space at the begging of the spline
-        void SpawnSegments();
+        //! Spawn segments with given rate of spawning
+        //! @param deltaTime the time since the last call of the function
+        void SpawnSegments(float deltaTime);
 
         AzPhysics::SceneEvents::OnSceneSimulationFinishHandler m_sceneFinishSimHandler; //!< Handler called after every physics sub-step
         AZStd::deque<AZStd::pair<float, AzPhysics::SimulatedBodyHandle>> m_ConveyorSegments; //!< Cache of created segments
@@ -112,5 +115,6 @@ namespace ROS2
         AZ::Data::Asset<Physics::MaterialAsset> m_materialAsset; //!<Material of individual segments of the conveyor belt
         AZ::EntityId m_ConveyorEntityId; //!< Conveyor belt entity (used for texture movement)
         float m_textureScale = 1.0f; //!< Scaling factor of the texture
+        float m_deltaTimeFromLastSpawn = 0.0f; //!< Time since the last spawn
     };
 } // namespace ROS2
