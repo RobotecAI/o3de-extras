b85e5473e2d667b3a1a66a4bde13a23dc0a7ec0b
Jan Hanca <jan.hanca@robotec.ai>
2024-01-19 10:36:36 +0100

    Fix spawner; rename spawner service (#654)
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

--- Gems/ROS2/Code/Source/Spawner/ROS2SpawnerComponent.cpp
+++ Gems/ROS2/Code/Source/Spawner/ROS2SpawnerComponent.cpp
@@ -34,7 +34,7 @@ namespace ROS2
         auto ros2Node = ROS2Interface::Get()->GetNode();
 
         m_getSpawnablesNamesService = ros2Node->create_service<gazebo_msgs::srv::GetWorldProperties>(
-            "get_available_spawnable_namespawnable_names",
+            "get_available_spawnable_names",
             [this](const GetAvailableSpawnableNamesRequest request, GetAvailableSpawnableNamesResponse response)
             {
                 GetAvailableSpawnableNames(request, response);
@@ -113,16 +113,13 @@ namespace ROS2
 
         SpawnEntityResponse response;
 
-        auto namespaceValidation = ROS2Names::ValidateNamespace(spawnableNamespace);
-        if (!namespaceValidation.IsSuccess())
+        if (auto namespaceValidation = ROS2Names::ValidateNamespace(spawnableNamespace); !namespaceValidation.IsSuccess())
         {
             response.success = false;
             response.status_message = namespaceValidation.GetError().data();
             service_handle->send_response(*header, response);
             return;
         }
-
-        auto spawnPoints = GetSpawnPoints();
 
         if (!m_controller.GetSpawnables().contains(spawnableName))
         {
@@ -155,7 +152,6 @@ namespace ROS2
         {
             // if a ticket for this spawnable was not created but the spawnable name is correct, create the ticket and then use it to
             // spawn an entity
-            auto spawnable = m_controller.GetSpawnables().find(spawnableName);
             m_tickets.emplace(spawnable->first, AzFramework::EntitySpawnTicket(spawnable->second));
         }
 
@@ -170,7 +166,7 @@ namespace ROS2
 
         AZ::Transform transform;
 
-        if (spawnPoints.contains(spawnPointName))
+        if (auto spawnPoints = GetSpawnPoints(); spawnPoints.contains(spawnPointName))
         {
             transform = spawnPoints.at(spawnPointName).pose;
         }
