780865e967f77549add94b568f412181003ee648
Norbert Prokopiuk <norbert.prokopiuk@robotec.ai>
2025-08-06 14:28:44 +0200

    Added disabling of services if it imposible to use them in Editor
    Signed-off-by: Norbert Prokopiuk <norbert.prokopiuk@robotec.ai>

--- Gems/SimulationInterfaces/Code/Source/Clients/ROS2SimulationInterfacesSystemComponent.cpp
+++ Gems/SimulationInterfaces/Code/Source/Clients/ROS2SimulationInterfacesSystemComponent.cpp
@@ -7,6 +7,7 @@
  */
 
 #include "ROS2SimulationInterfacesSystemComponent.h"
+#include <AzCore/Component/ComponentApplicationBus.h>
 
 #include <Actions/SimulateStepsActionServerHandler.h>
 #include <AzCore/Serialization/SerializeContext.h>
@@ -73,6 +74,9 @@ namespace ROS2SimulationInterfaces
     void ROS2SimulationInterfacesSystemComponent::Activate()
     {
         ROS2SimulationInterfacesRequestBus::Handler::BusConnect();
+        AZ::ApplicationTypeQuery appType;
+        AZ::ComponentApplicationBus::Broadcast(&AZ::ComponentApplicationBus::Events::QueryApplicationType, appType);
+        m_isAppEditor = (appType.IsValid() && appType.IsEditor());
 
         rclcpp::Node::SharedPtr ros2Node = rclcpp::Node::SharedPtr(ROS2::ROS2Interface::Get()->GetNode());
         AZ_Assert(ros2Node, "ROS2 node is not available.");
@@ -91,9 +95,14 @@ namespace ROS2SimulationInterfaces
         RegisterInterface<GetSimulationStateServiceHandler>(ros2Node);
         RegisterInterface<StepSimulationServiceHandler>(ros2Node);
         RegisterInterface<GetAvailableWorldsServiceHandler>(ros2Node);
-        RegisterInterface<GetCurrentWorldServiceHandler>(ros2Node);
-        RegisterInterface<LoadWorldServiceHandler>(ros2Node);
-        RegisterInterface<UnloadWorldServiceHandler>(ros2Node);
+        if (!m_isAppEditor)
+        {
+            // services related to world loading and unloading are available only in GameLauncher
+            // prevent creation of those service handlers
+            RegisterInterface<GetCurrentWorldServiceHandler>(ros2Node);
+            RegisterInterface<LoadWorldServiceHandler>(ros2Node);
+            RegisterInterface<UnloadWorldServiceHandler>(ros2Node);
+        }
     }
 
     void ROS2SimulationInterfacesSystemComponent::Deactivate()
--- Gems/SimulationInterfaces/Code/Source/Clients/ROS2SimulationInterfacesSystemComponent.h
+++ Gems/SimulationInterfaces/Code/Source/Clients/ROS2SimulationInterfacesSystemComponent.h
@@ -49,6 +49,8 @@ namespace ROS2SimulationInterfaces
     private:
         AZStd::unordered_map<AZStd::string, AZStd::shared_ptr<IROS2HandlerBase>> m_availableRos2Interface;
 
+        bool m_isAppEditor = false;
+        
         template<typename T>
         void RegisterInterface(rclcpp::Node::SharedPtr ros2Node)
         {
